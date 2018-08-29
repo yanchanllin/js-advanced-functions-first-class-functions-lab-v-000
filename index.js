55515f5f9bfa6ac408fa010049b250e2585874a2
YanC Lin <yonalin1@gmail.com>
2018-08-29 03:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+  return array.slice(0,2);
+}
+const returnLastTwoDrivers = function(array){
+  return array.slice(-2);
+}
